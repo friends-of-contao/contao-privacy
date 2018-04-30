98a25d59108b61e56eb031c6ee234fc3fc1ab2b5
Christian Barkowsky <hallo@christianbarkowsky.de>
2018-04-30 15:41:34 +0200

    #1

--- src/ContaoManager/Plugin.php
+++ src/ContaoManager/Plugin.php
@@ -10,6 +10,7 @@
 
 namespace Contao\PrivacyConsentBundle\ContaoManager;
 
+use Contao\CoreBundle\ContaoCoreBundle;
 use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
 use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
 use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
