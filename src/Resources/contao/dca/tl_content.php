12e6350ea45aeb39ae127285410c9547bce9596d
fritzmg <fmg@inspiredminds.at>
2018-05-15 13:24:19 +0200

    use height:auto

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -18,7 +18,7 @@ $GLOBALS['TL_DCA']['tl_content']['fields']['videoSplash'] = array
     'label'                   => &$GLOBALS['TL_LANG']['tl_content']['videoSplash'],
     'exclude'                 => true,
     'inputType'               => 'checkbox',
-    'eval'                    => array('submitOnChange'=>true, 'tl_class'=>'clr w50'),
+    'eval'                    => array('submitOnChange'=>true, 'tl_class'=>'clr w50', 'tl_style'=>'height:auto;'),
     'sql'                     => "char(1) NOT NULL default ''"
 );
 
