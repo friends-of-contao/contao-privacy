8052728045625a5b240717c23231d2e4af3b3406
Christian Barkowsky <hallo@christianbarkowsky.de>
2018-10-09 17:09:40 +0200

    #36

--- src/Resources/contao/dca/tl_module.php
+++ src/Resources/contao/dca/tl_module.php
@@ -53,3 +53,7 @@ $GLOBALS['TL_DCA']['tl_module']['palettes']['__selector__'][] = 'deleteCookies';
 $GLOBALS['TL_DCA']['tl_module']['subpalettes']['deleteCookies'] = 'keepCookies';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['analyticsOptOut'] = '{title_legend},name,headline,type;{config_legend},deleteCookies;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
 $GLOBALS['TL_DCA']['tl_module']['palettes']['focRegistration'] = '{title_legend},name,headline,type;{config_legend},editable,newsletters,disableCaptcha,focPrivacy,focPrivacyLabel;{account_legend},reg_groups,reg_allowLogin,reg_assignDir;{redirect_legend},jumpTo;{email_legend:hide},reg_activate;{template_legend:hide},memberTpl;{protected_legend:hide},protected;{expert_legend:hide},guests,cssID';
+
+if (\class_exists('\NotificationCenter\tl_form')) {
+    $GLOBALS['TL_DCA']['tl_module']['palettes']['focRegistration'] = str_replace('reg_activate;', 'reg_activate,nc_notification,nc_activation_notification;', $GLOBALS['TL_DCA']['tl_module']['palettes']['focRegistration']);
+}
